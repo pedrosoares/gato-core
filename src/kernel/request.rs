7d8f5f3491d14f0f2980400b64a9e2a7d221a73e
Pedro Soares <contato@pedrosoares.net>
2020-09-30 11:57:38 -0300

    Moved params to a single statement and inserted body in its place.

--- src/kernel/request.rs
+++ src/kernel/request.rs
@@ -5,11 +5,12 @@ pub struct Request {
     uri: String,
     headers: HashMap<String, String>,
     params: HashMap<String, String>,
+    body: HashMap<String, String>,
 }
 
 impl Request {
     pub fn new(method: String, uri: String, headers: HashMap<String, String>) -> Self {
-        return Request {method, uri, headers, params: HashMap::new()};
+        return Request {method, uri, headers, params: HashMap::new(), body: HashMap::new()};
     }
 
     pub fn get_uri(&self) -> String {
@@ -20,11 +21,22 @@ impl Request {
         self.params = params;
     }
 
+    pub fn add_body(&mut self, body: HashMap<String, String>) {
+        self.body = body;
+    }
+
     pub fn get_method(&self) -> String {
         return self.method.clone();
     }
 
     pub fn get(&self, name: &str, default_value: &str) -> String {
+        if self.body.contains_key(name) {
+            return self.body.get(&name.to_string()).unwrap().clone();
+        }
+        return default_value.to_string();
+    }
+
+    pub fn get_param(&self, name: &str, default_value: &str) -> String {
         if self.params.contains_key(name) {
             return self.params.get(&name.to_string()).unwrap().clone();
         }
@@ -32,7 +44,7 @@ impl Request {
     }
 
     pub fn all(&self) -> HashMap<String, String> {
-        return self.params.clone();
+        return self.body.clone();
     }
 
     pub fn get_headers(&self) -> HashMap<String, String> {
