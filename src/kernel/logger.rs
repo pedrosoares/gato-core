a10f8004a1385743c3a953b4ac26a0cb72af307f
Pedro Soares <contato@pedrosoares.net>
2020-10-14 22:41:46 -0300

    Logger now work with singleton to work with drivers.

--- src/kernel/logger.rs
+++ src/kernel/logger.rs
@@ -2,19 +2,42 @@
 use ::std::io::{Write};
 use std::fs::OpenOptions;
 use chrono::{Utc, Datelike, Timelike};
+use crate::kernel::singleton::{Singleton, new_singleton};
 
 pub struct Logger { }
 
 impl Logger {
 
+    pub fn set_driver(driver: Box<dyn Log>) {
+        unsafe {
+            LOGGER_DRIVER.set_instance(driver);
+        }
+    }
+
+    // TODO Implement Option Validation for Clean Message
+
+    fn get_driver() -> &'static Box<dyn Log> {
+        unsafe {
+            return LOGGER_DRIVER.get_instance();
+        }
+    }
+
     pub fn info(text: &str) -> () {
-        Logger::write_to_file("storage/logs/application.log", text);
+        let driver = Logger::get_driver();
+        driver.info(text);
     }
 
     pub fn error(text: &str) -> () {
-        Logger::write_to_file("storage/logs/errors.log", text);
+        let driver = Logger::get_driver();
+        driver.error(text);
     }
 
+
+}
+
+pub struct FileLogger {}
+
+impl FileLogger {
     fn write_to_file(file_name: &str, text: &str) -> () {
         let now = Utc::now();
         let date = format!("{}-{:02}-{:02} {:02}:{:02}:{:02}",
@@ -39,3 +62,24 @@ impl Logger {
         }
     }
 }
+
+impl Log for FileLogger {
+
+    fn info(&self, text: &str) {
+        FileLogger::write_to_file("storage/logs/application.log", text);
+    }
+
+    fn error(&self, text: &str) {
+        FileLogger::write_to_file("storage/logs/errors.log", text);
+    }
+
+}
+
+pub trait Log {
+
+    fn info(&self, text: &str);
+    fn error(&self, text: &str);
+
+}
+
+static mut LOGGER_DRIVER : Singleton<Box<dyn Log>> = new_singleton();
